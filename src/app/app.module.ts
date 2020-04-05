9d50b355af10b5e14b54ad325fc09872625db8ea
MohammadBnei <mohammadamine.banaei@gmail.com>
2020-04-05 15:53:31 +0200

    Functionnal angular Ivy app with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
 
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
+import {
+    BrowserAnimationsModule,
+} from '@angular/platform-browser/animations'
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+    declarations: [AppComponent, HomeComponent, AboutComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatToolbarModule
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
